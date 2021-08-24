3ae6ae079bae9ec95e7cb71deac1aa18deaaf715
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-08-23 23:12:05 -0700

    linked app.js to server

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || ;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./myapp/app.js')(app);
