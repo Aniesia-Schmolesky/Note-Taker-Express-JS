404b47a25334c1af41fc43ae1f0550a205ad04f8
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-08-24 18:42:25 -0700

    get api functions

--- Develop/myapp/app.js
+++ Develop/myapp/app.js
@@ -1,2 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+
+module.exports = app => {
+
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+
+        app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
+
+        app.post("/api/notes", function(req, res) {
+            let addNote = req.body;
+            notes.push(addNote);
+            updateDb();
+            return console.log("Add note: "+ addNote.title);
+        });
+   
