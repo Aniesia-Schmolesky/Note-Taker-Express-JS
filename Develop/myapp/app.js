50e52542bbaca2ddd0cfd87a9155f33ee311bf0d
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-08-24 18:55:11 -0700

    added send and write file functions

--- Develop/myapp/app.js
+++ Develop/myapp/app.js
@@ -6,13 +6,11 @@ module.exports = app => {
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
-
         var notes = JSON.parse(data);
 
         app.get("/api/notes", function(req, res) {
             res.json(notes);
         });
-
         app.post("/api/notes", function(req, res) {
             let addNote = req.body;
             notes.push(addNote);
@@ -24,3 +22,18 @@ module.exports = app => {
             res.json(notes[req.params.id]);
         });
    
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        function updateDb() {
+            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
+    });
